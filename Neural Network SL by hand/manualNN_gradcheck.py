5c7e9827839a221a10280958642b6b6b88828923
Leo Sutherby <leo.sutherby24@imperial.ac.uk>
2026-08-04 21:31:38 +0200

    added h(x) eval to manual NN
    slow going picking python back up - yet to add loss function, checking loss gradients, implementing training loop and checking gradients and results against pytorch

--- Neural Network SL by hand/manualNN_gradcheck.py
+++ Neural Network SL by hand/manualNN_gradcheck.py
@@ -3,6 +3,8 @@
 #NN structure hard coded in rather than adaptable in the interest of time
 
 import numpy as np
+import matplotlib.pyplot as plt
+from typing import NamedTuple
 
 #defining key parameters, this is hard coded in later at this stage, next step is to make code versatile
 n_layers = 3
@@ -17,35 +19,70 @@ y_array = rng.random(dataset_size)
 print("x:", x_array)
 print("y:", y_array)
 
+
+#augment x states
+# Create an array of 1.0s with the same length as your original array
+ones = np.ones_like(x_array, dtype=float)
+x_aug_vectors = np.vstack((x_array, ones))
+print("x_aug_vectors:", x_aug_vectors)
+
 #generate random NN weights
-w = rng.random(dimension_x+1)
+w = rng.random(2*dimension_x)
 U_1 = rng.random((n_width, (dimension_x+1)))
 U_2 = rng.random((n_width, (dimension_x+1)))
 print("w:", w)
 print("U_1:", U_1)
 print("U_2:", U_2)
 
-#augment x states
-x_aug_vectors = np.column_stack((x_array, np.ones_like(x_array)))
-print("x_aug_vectors:", x_aug_vectors)
+class Params(NamedTuple): #define a named tuple structure to store weight in 
+    w: np.ndarray   # (2x,)
+    U_1: np.ndarray   # (n_width,x+1)
+    U_2: np.ndarray   # (n_width,x+1)
+
+def set_params(w_in, U_1_in, U_2_in): #construct the weight tuple
+    return Params(
+        w = w_in,
+        U_1= U_1_in,
+        U_2= U_2_in,
+    )
+
+params = set_params(w, U_1, U_2)
+
 
 #define non_linear activation function
-def sigma(A) #using tanh for now, need a smooth function for gradient check accuracy
-    sigmaval = np.tanh(A) #what is the dimension of A?? how do I make act elementwise??
-    return sigmaval
+def sigma(A): #using tanh for now, need a smooth function for gradient check accuracy
+    sigmaval = np.tanh(A) 
+    return sigmaval #vector x long
 
 #define h(x) eval
-def h(x)
-
-
-
+def h(x, params):
+    phi_d = sigma(params.U_2@x)
+    phi = sigma(params.U_1@phi_d)
+    hval = ((params.w).T)@phi
     return hval
+
+#evaluate h(x) manually
+hval = h(x_aug_vectors[:,0], params)
+
+#plot h(x) estimate
+def ploth(params):
+    x_grid = np.linspace(0.0, 1.0, 50)
+    h_array = []
+    for i in range(50):
+        xval_aug = np.array([[x_grid[i]], [1.0]])
+        h_array.append(h(xval_aug, params))
+    plt.scatter(x_array, y_array)
+    plt.plot(x_grid, h_array)
+    plt.show()  
+
+ploth(params)
+    
 
 #define loss function
 
 
 
-#evaluate h(x) manually
+
 
 
 #loss function definition
