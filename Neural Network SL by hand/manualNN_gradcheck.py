96c95642bab260cdd9d5b4ae60d68d60bb4067c5
Leo Sutherby <leo.sutherby24@imperial.ac.uk>
2026-08-08 15:52:46 +0200

    loss function + loss gradient via finite differences
    deep in backprop chain rule algebra, will implement soon to be able to compare

--- Neural Network SL by hand/manualNN_gradcheck.py
+++ Neural Network SL by hand/manualNN_gradcheck.py
@@ -1,5 +1,4 @@
-#manually coded neural network with forward pass(h(x) eval) and cost gradient compared between chain rule and finite differences
-#check theory understanding and refresh python
+#manually coded neural network with forward pass(h(x) eval) and cost gradients found in 3 ways: finite difference, manual chain rule backprop algebra, backprop algorithm (reverse mode AD) (last skipped for now but will be returned to)
 #NN structure hard coded in rather than adaptable in the interest of time
 
 import numpy as np
@@ -48,7 +47,6 @@ def set_params(w_in, U_1_in, U_2_in): #construct the weight tuple
 
 params = set_params(w, U_1, U_2)
 
-
 #define non_linear activation function
 def sigma(A): #using tanh for now, need a smooth function for gradient check accuracy
     sigmaval = np.tanh(A) 
@@ -75,25 +73,84 @@ def ploth(params):
     plt.plot(x_grid, h_array)
     plt.show()  
 
-ploth(params)
+#ploth(params)
     
+#define loss function of h(x) across D_TR - squared loss, no regularizer
+def Loss(params, x_array, y_array): 
+    loss = 0.0
+    for i in range(dataset_size):
+        xval_aug = np.array([[x_array[i]], [1.0]])
+        loss += (h(xval_aug, params) - y_array[i])**2
+    loss = loss/dataset_size
+    return loss
 
-#define loss function
-
-
-
-
-
-
-#loss function definition
-
-
-
-#evaluate manual NN gradients via chain rule
+print("loss = ", Loss(params, x_array, y_array))
 
 
+#Find loss gradients w.r.t. parameters:
 
-#evaluate manual NN gradients via finite differencing
+#1) evaluate manual NN gradients via finite differencing
+eps = 1e-12
+
+dLdw_fdiff = np.zeros((2*dimension_x, 1), dtype=np.float64)
+for i in range(2*dimension_x):
+    
+    w_lower = params.w.copy() #make a copy otherwise changes to w_lower would also change w
+    w_lower[i] = w_lower[i] - eps
+    params_lower = set_params(w_lower, U_1, U_2)
+
+    w_upper = params.w.copy() #make a copy otherwise changes to w_lower would also change w
+    w_upper[i] = w_upper[i] + eps
+    params_upper = set_params(w_upper, U_1, U_2)
+
+    dLdw_fdiff[i] = (Loss(params_upper, x_array, y_array) - Loss(params_lower, x_array, y_array))/(2.0*eps)
+
+print("dLdw_fdiff", dLdw_fdiff)
+
+
+dLdU1_fdiff = np.zeros((n_width, (dimension_x+1)), dtype=np.float64)
+
+for column in range((dimension_x+1)):
+    for row in range(n_width):
+
+        U_1_lower = params.U_1.copy()
+        U_1_lower[row, column] = U_1_lower[row, column] - eps
+        params_lower = set_params(w, U_1_lower, U_2)
+
+        U_1_upper = params.U_1.copy()
+        U_1_upper[row, column] = U_1_upper[row, column] + eps
+        params_upper = set_params(w, U_1_upper, U_2)
+
+        dLdU1_fdiff[row, column] = ((Loss(params_upper, x_array, y_array) - Loss(params_lower, x_array, y_array))/(2.0*eps)).item()
+
+print("dLdU1_fdiff", dLdU1_fdiff)
+
+dLdU2_fdiff = np.zeros((n_width, (dimension_x+1)), dtype=np.float64)
+
+for column in range((dimension_x+1)):
+    for row in range(n_width):
+
+        U_2_lower = params.U_2.copy()
+        U_2_lower[row, column] = U_2_lower[row, column] - eps
+        params_lower = set_params(w, U_1, U_2_lower)
+
+        U_2_upper = params.U_2.copy()
+        U_2_upper[row, column] = U_2_upper[row, column] + eps
+        params_upper = set_params(w, U_1, U_2_upper)
+
+        dLdU2_fdiff[row, column] = ((Loss(params_upper, x_array, y_array) - Loss(params_lower, x_array, y_array))/(2.0*eps)).item()
+
+print("dLdU2_fdiff", dLdU2_fdiff)
+
+
+#2) evaluate manual NN gradients via chain rule algebraically
+
+
+#3) evaluate NN gradients backprop algorithmically (reverse mode auto diff)- mirroring actual modern process
+#skipped for this project in interest of time but will return once theory is necessary
+
+
+
 
 
 
